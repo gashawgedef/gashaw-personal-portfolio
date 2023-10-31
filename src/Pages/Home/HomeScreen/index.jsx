24fa956198fd57993c6bd23a3c6d136ceeeb37ca
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-10-31 16:42:40 -0700

    gashaw gedef

--- src/Pages/Home/HomeScreen/index.jsx
+++ src/Pages/Home/HomeScreen/index.jsx
@@ -1,11 +1,12 @@
 import HeroSection from "../HeroSection";
-
 import React from 'react'
+import MySkills from "../MySkills";
 
 const index = () => {
   return (
     <div>
         <HeroSection/>
+        <MySkills/>
       
     </div>
   )
