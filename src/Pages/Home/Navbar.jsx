27042780e757cb70f3366494ad5a375bf3c9d476
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-10-31 15:03:19 -0700

    my front end app

--- src/Pages/Home/Navbar.jsx
+++ src/Pages/Home/Navbar.jsx
@@ -1,46 +1,114 @@
 
 
 // import React from 'react'
-import { useState,useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { Link } from 'react-scroll'
 
-function Navbar () {
+function Navbar() {
 
-  const [navActive,setNavActive]=useState(false)
+    const [navActive, setNavActive] = useState(false)
 
-  const toggleNav=()=>{
-    setNavActive(!navActive)
-  }
-  const closeMenu=()=>{
-    setNavActive(false)
-  }
-  useEffect(()=>{
-    const handleResize=()=>{
-        if(window.innerWidth<=500){
+    const toggleNav = () => {
+        setNavActive(!navActive)
+    }
+    const closeMenu = () => {
+        setNavActive(false)
+    }
+    useEffect(() => {
+        const handleResize = () => {
+            if (window.innerWidth <= 500) {
+                closeMenu
+            }
+        }
+        window.addEventListener("resize", handleResize);
+
+        return () => {
+            window.removeEventListener("resize", handleResize)
+        }
+    }, []);
+
+    useEffect(() => {
+        if (window.innerWidth <= 1200) {
             closeMenu
         }
-    }
-    window.addEventListener("resize",handleResize);
+    }, []);
 
-    return ()=>{
-        window.removeEventListener("resize",handleResize)
-    }
-  },[]);
+    return (
+        <nav className={`navbar ${navActive ? "active" : ""}`}>
+            <div>
+                <img src="./img/logo.svg" alt="Gashaw Gedef" />
+            </div>
+            <a className={`nav__hamburger ${navActive ? "active" : ""}`} onClick={toggleNav}>
+                <span className="nav__hamburger__line"></span>
+                <span className="nav__hamburger__line"></span>
+                <span className="nav__hamburger__line"></span>
+            </a>
+            <div className={`navbar--items ${navActive ? "active" : ""}`}>
+                <ul>
+                    <li>
+                        <Link
+                            onClick={closeMenu}
+                            activeClass='navbar--active-content'
+                            spy={true} smooth={true}
+                            offset={-70} duration={500}
+                            to="heroSection" className='navbar--content'
+                        >
+                            Home
+                        </Link>
+                    </li>
 
-  useEffect(()=>{
-    if(window.innerWidth<=1200){
-        closeMenu
-    }
-  },[]);
+                    <li>
+                        <Link
+                            onClick={closeMenu}
+                            activeClass='navbar--active-content'
+                            spy={true} smooth={true}
+                            offset={-70} duration={500}
+                            to="MyPortfolio" className='navbar--content'
+                        >
+                            Portfolio
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            onClick={closeMenu}
+                            activeClass='navbar--active-content'
+                            spy={true} smooth={true}
+                            offset={-70} duration={500}
+                            to="AboutMe" className='navbar--content'
+                        >
+                            About Me
+                        </Link>
+                    </li>
+                    <li>
+                        <Link
+                            onClick={closeMenu}
+                            activeClass='navbar--active-content'
+                            spy={true} smooth={true}
+                            offset={-70} duration={500}
+                            to="Testmonials" className='navbar--content'
+                        >
+                            Testimonials
+                        </Link>
 
-  return(
-    <nav className={`navbar ${navActive?"active":""}`}>
-        <div>
-            <img src="./img/logo.svg" alt="Gashaw Gedef" />
-        </div>
+                    </li>
+                </ul>
 
-    </nav>
-  )
+            </div>
+            <Link
+                onClick={closeMenu}
+                activeClass="navbar--active-content"
+                spy={true}
+                smooth={true}
+                offset={-70}
+                duration={500}
+                to="Contact"
+                className="btn btn-outline-primary"
+            >
+                Contact Me
+            </Link>
+        </nav>
+
+    )
 
 }
 
