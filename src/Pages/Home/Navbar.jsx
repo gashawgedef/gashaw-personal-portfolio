8a0f40d9721421a8487debba2068751a6045358f
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-10-31 13:50:18 -0700

    nav structure

---
+++ src/Pages/Home/Navbar.jsx
@@ -0,0 +1,38 @@
+
+
+// import React from 'react'
+import { useState,useEffect } from 'react'
+import { Link } from 'react-scroll'
+
+function Navbar () {
+
+  const [navActive,setNavActive]=useState(false)
+
+  const toggleNav=()=>{
+    setNavActive(!navActive)
+  }
+  const closeMenu=()=>{
+    setNavActive(false)
+  }
+  useEffect(()=>{
+    const handleResize=()=>{
+        if(window.innerWidth<=500){
+            closeMenu
+        }
+    }
+    window.addEventListener("resize",handleResize);
+
+    return ()=>{
+        window.removeEventListener("resize",handleResize)
+    }
+  },[]);
+
+  useEffect(()=>{
+    if(window.innerWidth<=1200){
+        closeMenu
+    }
+  },[]);
+
+}
+
+export default Navbar
