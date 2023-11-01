f147137c4b58fa9b42a738b410d587ab2c7f3146
gashawgedef@gmail.com <gashawgedef@gmail.com>
2023-11-01 09:49:37 -0700

    The commit message for react front end app

--- src/Pages/Home/HomeScreen/index.jsx
+++ src/Pages/Home/HomeScreen/index.jsx
@@ -1,13 +1,22 @@
 import HeroSection from "../HeroSection";
 import React from 'react'
 import MySkills from "../MySkills";
+import AboutMe from "./About";
+import MyPortfolio from "../MyPortfolio";
+import Testimonial from "../Testinomials";
+import ContactMe from "../ContactMe";
+import Footer from "../Footer";
 
 const index = () => {
   return (
     <div>
         <HeroSection/>
         <MySkills/>
-      
+        <AboutMe/>
+        <MyPortfolio/>
+      <Testimonial/>
+      <ContactMe/>
+      <Footer/>
     </div>
   )
 }
